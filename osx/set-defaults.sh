a9d835073652c7f4553d7e5161b146ceaea96baf
Simon Whitaker <simon@goosoftware.co.uk>
2012-12-31 19:14:07 +0000

    Comment out line to disable press-and-hold

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -4,5 +4,8 @@
 chflags nohidden ~/Library
 
 # Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
+#defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Don't add drop shadows to screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
 
