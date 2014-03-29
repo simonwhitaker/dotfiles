442405c1188fdabbd2fc24418ec4affc770e9c63
Simon Whitaker <sw@netcetera.org>
2014-03-29 20:58:32 +0000

    Add simplified install routine

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Get absolute path to dotfiles dir (see http://stackoverflow.com/q/242538/263871; readlink -f doesn't work on OSX)
+pushd $(dirname "$0") > /dev/null
+DOTFILES_DIR=$(pwd -L)
+popd > /dev/null
+
+
+link() {
+    ln -sf "$DOTFILES_DIR/$1" "$HOME/.$(basename $1)"
+}
+
+link git/gitconfig
+link git/gitignore
+link lldb/lldbinit
+link vim/vimrc
+link zsh/zshrc
+link zsh/zsh-plugins
---
+++ zsh/zsh-plugins/python.zsh
@@ -0,0 +1,12 @@
+
+
+export PIP_DOWNLOAD_CACHE=$HOME/.pip-download-cache
+
+
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+export WORKON_HOME=$HOME/.virtualenvs
+vew_bootstrap=/usr/local/bin/virtualenvwrapper.sh
+if [[ -a $vew_bootstrap ]]
+then
+    source $vew_bootstrap
+fi
