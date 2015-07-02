f258c5b5f15c48228459c91bde602ea33ac769a3
Simon Whitaker <sw@netcetera.org>
2015-07-02 08:02:33 -0700

    Fix hg aliases

--- zsh/zsh-plugins/mercurial.zsh
+++ zsh/zsh-plugins/mercurial.zsh
@@ -13,8 +13,8 @@ alias hglr='hg pull --rebase'
 alias hgo='hg outgoing'
 alias hgp='hg push'
 alias hgs='hg status'
-alias hl='hg log --limit 20 --template "{node|short} | {date|isodatesec} | {author|user}: {desc|strip|firstline}\n" '
-alias hgll='hg log  -T "{label(\"magenta\", node|short)} {date|localdate|isodate} {if(phabdiff, label(\"green\", phabdiff), \"(no diff)\")} {label(\"yellow\", author|user)} {desc|firstline}\n"'
+alias hgsl='hg log --limit 20 --template "{node|short} | {date|isodatesec} | {author|user}: {desc|strip|firstline}\n" '
+alias hl='hg log  -T "{label(\"magenta\", node|short)} {date|localdate|isodate} {if(phabdiff, label(\"green\", phabdiff), \"(no diff)\")} {label(\"yellow\", author|user)} {desc|firstline}\n"'
 # this is the 'git commit --amend' equivalent
 alias hgca='hg qimport -r tip ; hg qrefresh -e ; hg qfinish tip'
 # list unresolved files (since hg does not list unmerged files in the status command)
