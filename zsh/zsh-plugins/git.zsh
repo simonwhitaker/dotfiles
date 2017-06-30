282e8dea67dc57fbb51d871f4572adc5664fb014
Simon Whitaker <sw@netcetera.org>
2017-06-30 19:28:58 +0100

    Comment out some git aliases

--- zsh/zsh-plugins/git.zsh
+++ zsh/zsh-plugins/git.zsh
@@ -77,16 +77,16 @@ alias gss='git status -s'
 compdef _git gss=git-status
 alias ga='git add'
 compdef _git ga=git-add
-alias gm='git merge'
-compdef _git gm=git-merge
-alias grh='git reset HEAD'
-alias grhh='git reset HEAD --hard'
-alias gclean='git reset --hard && git clean -dfx'
-alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+# alias gm='git merge'
+# compdef _git gm=git-merge
+# alias grh='git reset HEAD'
+# alias grhh='git reset HEAD --hard'
+# alias gclean='git reset --hard && git clean -dfx'
+# alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
 
 alias gpoat='git push origin --all && git push origin --tags'
-alias gmt='git mergetool --no-prompt'
-compdef _git gm=git-mergetool
+# alias gmt='git mergetool --no-prompt'
+# compdef _git gm=git-mergetool
 
 alias gg='git gui citool'
 alias gga='git gui citool --amend'
