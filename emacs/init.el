982cab49ccf1936a41afb369907755fba1e508ae
Simon Whitaker <sw@netcetera.org>
2017-07-14 09:36:22 +0100

    Make file backups in emacs sane

--- emacs/init.el
+++ emacs/init.el
@@ -5,6 +5,16 @@
 	     '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
+;; Backup foo from https://www.emacswiki.org/emacs/BackupDirectory
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.emacs.d/backups"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
 ;; Turn off the menu bar
 (menu-bar-mode -1)
 (custom-set-variables
