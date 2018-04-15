04e5d6fac94098e1a0c866c66257ad471a450c66
Simon Whitaker <sw@netcetera.org>
2018-04-15 10:13:14 +0100

    Remove unused junk

--- zsh/zsh-plugins/osx.zsh
+++ zsh/zsh-plugins/osx.zsh
@@ -1,20 +1,9 @@
 if [[ -a /Applications ]]
 then
     # OS X-specific stuff
-    alias tm="mate"
-    alias tmm="mate ."
-    alias zshrc="subl ~/.zshrc"
-    # alias ohmyzsh="subl ~/.oh-my-zsh"
     alias pljson="plutil -convert json -r -o -"
-    alias st="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
-    alias stt="st ."
     alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
     alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
-
-    kill-adobe-updater() {
-        killall "AAM Updates Notifier" 2>/dev/null || echo "AAM Updater not running"
-        rm ~/Library/LaunchAgents/com.adobe.AAM.Updater-1.0.plist 2>/dev/null || echo "LaunchAgent not found"
-    }
 
     function man-preview() {
         man -t "$@" | open -f -a Preview
