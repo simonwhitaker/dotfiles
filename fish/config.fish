38d896f0c11c35f06a3ed8b9a58e4795ecfce6f9
Simon Whitaker <sw@netcetera.org>
2018-04-16 09:44:07 +0100

    cd to project dir when activating virtualenv

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,12 @@
 eval (python -m virtualfish)
 
+# cd to project directory on activating a virtualenv
+function switch_to_virtualenv_directory --on-event virtualenv_did_activate
+    if test -f $VIRTUAL_ENV/.project
+        cd (cat $VIRTUAL_ENV/.project)
+    end
+end
+
 set __sw_cargo_bin "$HOME/.cargo/bin"
 if test -d $__sw_cargo_bin
    set PATH $__sw_cargo_bin $PATH
