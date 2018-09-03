dbea1322b3a0a0edfcb8765285c3ff87b3e3bf33
Simon Whitaker <sw@netcetera.org>
2018-09-03 10:27:37 +0100

    Add GOPATH

--- fish/config.fish
+++ fish/config.fish
@@ -16,5 +16,8 @@ set -gx EDITOR (type -p emacs)
 
 set -gx DOTFILES $HOME/src/misc/dotfiles
 
+set -gx GOPATH $HOME/src/go
+set PATH $GOPATH/bin $PATH
+
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/simon/Library/Developer/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/simon/Library/Developer/google-cloud-sdk/path.fish.inc'; else; . '/Users/simon/Library/Developer/google-cloud-sdk/path.fish.inc'; end; end
