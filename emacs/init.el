77f4c691bb27eddf46e8d92fecfdd4defe265dee
Simon Whitaker <sw@netcetera.org>
2019-04-08 10:27:18 +0100

    Misc changes

--- emacs/init.el
+++ emacs/init.el
@@ -15,6 +15,11 @@
    kept-old-versions 2
    version-control t)       ; use versioned backups
 
+;; Tell emacs I'm using a dark terminal background
+;; See https://www.reddit.com/r/emacs/comments/67110h/emacs_terminal_colors_are_barely_visible/
+(setq
+ frame-background-mode 'dark)
+
 ;; Turn off the menu bar
 (menu-bar-mode -1)
 (custom-set-variables
@@ -22,10 +27,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (rust-mode swift-mode markdown-mode))))
+ '(package-selected-packages
+   (quote
+    (yaml-mode go-mode rust-mode swift-mode markdown-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
