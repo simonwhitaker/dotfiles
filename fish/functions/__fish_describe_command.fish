0ab2875b452c12184a386bbb6bea5aaf48423674
Simon Whitaker <sw@netcetera.org>
2020-12-31 12:39:42 +0000

    Speed up tab completion on macOS

---
+++ fish/functions/__fish_describe_command.fish
@@ -0,0 +1,4 @@
+# Defined in /var/folders/xv/dxvtxf4j5ys_7r2fcf7_85p40000gn/T//fish.qrSF96/__fish_describe_command.fish @ line 2
+function __fish_describe_command --description 'Command used to find descriptions for commands'
+# disabled to speed up tab autocompletion, see https://github.com/fish-shell/fish-shell/issues/6270#issuecomment-548515306
+end
