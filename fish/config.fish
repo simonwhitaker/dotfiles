c03ae96fb56b33f50bd919e002271ec4b135ef82
Simon Whitaker <s.whitaker@synthace.com>
2021-03-11 16:41:06 +0000

    Get rid of special logic for config.local.fish, use conf.d instead

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+# This config is used across all my machines, synced via my dotfiles git repo.
+#
+# To add config for the local machine only, add a file to ~/.config/fish/conf.d,
+# e.g. ~/.config/fish/conf.d/local.fish
+
 # cd to project directory on activating a virtualenv
 function switch_to_virtualenv_directory --on-event virtualenv_did_activate
     if test -f "$VIRTUAL_ENV/.project"
@@ -10,11 +15,6 @@ set -gx DOTFILES $HOME/src/misc/dotfiles
 
 set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
 set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
-
-set local_config ~/.config/fish/config.local.fish
-if test -f $local_config
-    source $local_config
-end
 
 fish_add_path "$HOME/.cargo/bin"
 fish_add_path "$DOTFILES/bin"
