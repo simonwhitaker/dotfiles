74fd295c39e00a893662f017bc5c25b90975f3b4
Simon Whitaker <sw@netcetera.org>
2021-08-07 19:44:56 +0100

    osx: configure hot corners

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -7,5 +7,17 @@ chflags nohidden ~/Library
 #defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Don't add drop shadows to screenshots
-defaults write com.apple.screencapture disable-shadow -bool true
+# defaults write com.apple.screencapture disable-shadow -bool true
 
+# Hot Corners
+# Bottom left: Put display to sleep
+defaults write com.apple.dock wvous-bl-corner -int 10
+# Bottom right: Desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+# Top left: Mission Control
+defaults write com.apple.dock wvous-tl-corner -int 2
+# Top right: Notification Centre
+defaults write com.apple.dock wvous-tr-corner -int 12
+
+# Restart the dock to pick up hot corner changes
+pkill Dock
