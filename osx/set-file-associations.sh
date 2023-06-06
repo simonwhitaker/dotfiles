4cc4c03b7e96cf99218a6479f124b6bb50e1452a
Simon Whitaker <sw@netcetera.org>
2023-06-06 20:41:21 +0100

    Add script to set file associations

---
+++ osx/set-file-associations.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Adapted from https://stackoverflow.com/a/70402997/263871
+
+brew install duti
+
+# Use duti to set defaults for specific files to VSCode
+duti -s com.microsoft.VSCode public.plain-text all
+duti -s com.microsoft.VSCode public.source-code all
+duti -s com.microsoft.VSCode public.data all
+duti -s com.microsoft.VSCode .css all
+duti -s com.microsoft.VSCode .gitattributes all
+duti -s com.microsoft.VSCode .gitignore all
+duti -s com.microsoft.VSCode .htaccess all
+duti -s com.microsoft.VSCode .js all
+duti -s com.microsoft.VSCode .json all
+duti -s com.microsoft.VSCode .link all
+duti -s com.microsoft.VSCode .md all
+duti -s com.microsoft.VSCode .mv all
+duti -s com.microsoft.VSCode .mvt all
+duti -s com.microsoft.VSCode .scss all
+duti -s com.microsoft.VSCode .sh all
+duti -s com.microsoft.VSCode .txt all
+duti -s com.microsoft.VSCode .xml all
+duti -s com.microsoft.VSCode .yaml all
+duti -s com.microsoft.VSCode .zsh all
