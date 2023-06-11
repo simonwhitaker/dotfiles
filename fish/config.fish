9334fb0f170e0a427ffae1a0f30cf29a3040c2fe
Simon Whitaker <simon.whitaker@klaviyo.com>
2023-06-11 13:35:29 +0100

    Also bootstrap pyenv-virtualenv if available

--- fish/config.fish
+++ fish/config.fish
@@ -43,6 +43,10 @@ if type -q pyenv
     set -gx PYENV_ROOT $HOME/.pyenv
     fish_add_path $PYENV_ROOT/bin
     pyenv init - | source
+
+    if type -q pyenv-virtualenv
+          pyenv virtualenv-init - | source
+    end
 end
 
 if type -q op
