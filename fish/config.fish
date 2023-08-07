a00ffcba968fbab6a96f893070a3d661a7d21898
Simon Whitaker <simon.whitaker@klaviyo.com>
2023-08-07 16:30:36 +0100

    Remove dynamic completion generation
    It's slow, and some (op, ngrok) are installed on install/upgrade

--- fish/config.fish
+++ fish/config.fish
@@ -49,23 +49,6 @@ if type -q pyenv
     end
 end
 
-if type -q op
-    op completion fish | source
-end
-
-if type -q ngrok
-    ngrok completion | source
-end
-
-if type -q devpod
-    devpod completion fish | source
-end
-
-if type -q gibo-go
-    gibo-go completion fish | source
-end
-
-
 # https://github.com/sharkdp/bat#customization
 export BAT_THEME="Monokai Extended Bright"
 
