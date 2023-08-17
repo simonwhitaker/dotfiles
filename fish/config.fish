ca67db46094a6a17f031710c1793fd1b1212b53f
Simon Whitaker <sw@netcetera.org>
2023-08-17 09:36:11 +0100

    Remove brew shellenv from fish.config
    Use `/path/to/brew shellenv >> ~/.config/fish/conf.d/brew.fish` instead

--- fish/config.fish
+++ fish/config.fish
@@ -22,22 +22,9 @@ set dotfiles_fishdir (dirname (realpath (status filename)))
 set dotfiles (git -C "$dotfiles_fishdir" rev-parse --show-toplevel)
 fish_add_path "$dotfiles/bin"
 
-# Intel-specific Homebrew stuff. (On Apple Silicon, the Homebrew prefix is
-# /opt/homebrew.)
-# set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
-# set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
-# fish_add_path "/usr/local/sbin"
-
 fish_add_path "$HOME/.cargo/bin"
 fish_add_path "$HOME/go/bin"
 fish_add_path "$HOME/.local/bin" # Used by poetry
-
-# Homebrew installs in /usr/local on Intel, /opt/homebrew on Apple Silicon (see
-# https://docs.brew.sh/Installation)
-if test -d /opt/homebrew
-    # Set PATH, MANPATH, etc., for Homebrew.
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-end
 
 if type -q pyenv
     set -gx PYENV_ROOT $HOME/.pyenv
