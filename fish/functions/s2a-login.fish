b09ff546953247e6e1d2e2b0a2877798f0edfd60
Simon Whitaker <simon.whitaker@klaviyo.com>
2023-09-13 06:14:01 +0100

    Tweak s2a-login

--- fish/functions/s2a-login.fish
+++ fish/functions/s2a-login.fish
@@ -3,7 +3,7 @@ function s2a-login --description "Log in to AWS for this session only"
     set -x SAML2AWS_CREDENTIALS_FILE (mktemp)
     if saml2aws login --force --skip-prompt --cache-saml $argv
         set _aws_role (grep x_principal_arn $SAML2AWS_CREDENTIALS_FILE 2>/dev/null | cut -d/ -f2)
-        saml2aws script | source
+        saml2aws script --shell fish | source
         set -gx S2A_AWS_ROLE "$_aws_role"
     end
     rm -rf $SAML2AWS_CREDENTIALS_FILE
