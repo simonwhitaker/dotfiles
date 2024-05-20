500a67de47c22f495aaeb9d4cef17b0acfe9568e
Simon Whitaker <sw@netcetera.org>
2024-05-20 18:43:06 +0100

    Add description to code.fish

--- fish/functions/code.fish
+++ fish/functions/code.fish
@@ -2,6 +2,6 @@
 # https://github.com/microsoft/vscode/issues/60579#issuecomment-434583718 for
 # the issue where invoking `code` from the command line results in multiple
 # VSCode icons in the MacOS dock.
-function code
+function code --description "Open the files passed as arguments in VS Code"
   open -b com.microsoft.VSCode "$argv"
 end
