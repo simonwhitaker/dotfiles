908356c948f9b3630c1d86ac64ff1adbd87fe3a9
Simon Whitaker <sw@netcetera.org>
2024-11-12 21:53:20 +0000

    Use Cursor as the preferred code variant, if installed

--- fish/functions/code.fish
+++ fish/functions/code.fish
@@ -2,6 +2,6 @@
 # https://github.com/microsoft/vscode/issues/60579#issuecomment-434583718 for
 # the issue where invoking `code` from the command line results in multiple
 # VSCode icons in the MacOS dock.
-function code --description "Open the files passed as arguments in VS Code"
-  open -b com.microsoft.VSCode "$argv"
+function code --description "Open the files passed as arguments in Cursor if installed, else in VS Code"
+  open -b com.todesktop.230313mzl4w4u92 "$argv" 2>/dev/null || open -b com.microsoft.VSCode "$argv"
 end
