9cc22a543bbf9684dc8ab94afcabde27ed6c7b66
Simon Whitaker <sw@netcetera.org>
2025-06-10 19:25:27 +0100

    Remove old orbstack line

--- fish/config.fish
+++ fish/config.fish
@@ -98,8 +98,4 @@ abbr -a za 'zellij attach'
 
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
-source ~/.orbstack/shell/init.fish 2>/dev/null || :
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
