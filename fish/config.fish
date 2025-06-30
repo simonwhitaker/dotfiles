001bfa36387092d2f9810afadedf6435ac3c2717
Simon Whitaker <sw@netcetera.org>
2025-06-30 11:47:42 +0100

    Add alias for local claude installation

--- fish/config.fish
+++ fish/config.fish
@@ -99,3 +99,5 @@ abbr -a za 'zellij attach'
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+alias claude="/Users/simon/.claude/local/claude"
