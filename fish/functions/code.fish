0a1419e41470ee448f89d6b7a10c029248991a56
Simon Whitaker <sw@netcetera.org>
2025-08-14 14:53:25 +0100

    Improve `code` function to try multiple editors in order

--- fish/functions/code.fish
+++ fish/functions/code.fish
@@ -3,5 +3,14 @@
 # the issue where invoking `code` from the command line results in multiple
 # VSCode icons in the MacOS dock.
 function code --description "Open the files passed as arguments in Cursor if installed, else in VS Code"
-  open -b com.todesktop.230313mzl4w4u92 "$argv" 2>/dev/null || open -b com.microsoft.VSCode "$argv"
+    set editor_bundle_ids \
+        dev.zed.Zed \
+        com.todesktop.230313mzl4w4u92 \
+        com.microsoft.VSCode
+    for editor_bundle_id in $editor_bundle_ids
+        if open -b $editor_bundle_id "$argv" 2>/dev/null
+            return 0
+        end
+    end
+    return 1
 end
