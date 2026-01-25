e7ee2b7cb38b2e5ddd1036972276cc2aa5413b9a
Simon Whitaker <sw@netcetera.org>
2026-01-25 12:09:42 +0000

    osx: use Zed as default text editor

--- osx/set-file-associations.sh
+++ osx/set-file-associations.sh
@@ -4,26 +4,29 @@
 
 brew install duti
 
+bundle_id=dev.zed.Zed
+# bundle_id=com.microsoft.VSCode
+
 # Use duti to set defaults for specific files to VSCode
-duti -s com.microsoft.VSCode public.plain-text all
-duti -s com.microsoft.VSCode public.source-code all
-duti -s com.microsoft.VSCode public.data all
-duti -s com.microsoft.VSCode .css all
-duti -s com.microsoft.VSCode .gitattributes all
-duti -s com.microsoft.VSCode .gitignore all
-duti -s com.microsoft.VSCode .htaccess all
-duti -s com.microsoft.VSCode .js all
-duti -s com.microsoft.VSCode .js all
-duti -s com.microsoft.VSCode .json all
-duti -s com.microsoft.VSCode .link all
-duti -s com.microsoft.VSCode .md all
-duti -s com.microsoft.VSCode .mv all
-duti -s com.microsoft.VSCode .mvt all
-duti -s com.microsoft.VSCode .py all
-duti -s com.microsoft.VSCode .scss all
-duti -s com.microsoft.VSCode .sh all
-duti -s com.microsoft.VSCode .ts all
-duti -s com.microsoft.VSCode .txt all
-duti -s com.microsoft.VSCode .xml all
-duti -s com.microsoft.VSCode .yaml all
-duti -s com.microsoft.VSCode .zsh all
+duti -s $bundle_id public.plain-text all
+duti -s $bundle_id public.source-code all
+duti -s $bundle_id public.data all
+duti -s $bundle_id .css all
+duti -s $bundle_id .gitattributes all
+duti -s $bundle_id .gitignore all
+duti -s $bundle_id .htaccess all
+duti -s $bundle_id .js all
+duti -s $bundle_id .js all
+duti -s $bundle_id .json all
+duti -s $bundle_id .link all
+duti -s $bundle_id .md all
+duti -s $bundle_id .mv all
+duti -s $bundle_id .mvt all
+duti -s $bundle_id .py all
+duti -s $bundle_id .scss all
+duti -s $bundle_id .sh all
+duti -s $bundle_id .ts all
+duti -s $bundle_id .txt all
+duti -s $bundle_id .xml all
+duti -s $bundle_id .yaml all
+duti -s $bundle_id .zsh all
